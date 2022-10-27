87b7bd8d0d41fbe84e8d06d2bfdb831a45fbf74a
listingslab <listingslab@gmail.com>
2022-10-27 04:48:47 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
+import * as React from "react";
+import type { HeadFC } from "gatsby";
 
 const IndexPage = () => {
   return (
     <main>
-      IndexPage
+      Wei Zang
     </main>
   )
 }
 
-export default IndexPage
+export default IndexPage;
 
 export const Head: HeadFC = () => {
-  return <title>HEAD FC</title>;
+  return <title>Wei Zang</title>;
 }
